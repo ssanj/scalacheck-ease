9fc70b712d3ad24924526efb37321cf086f56c93
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-10-18 00:11:18 +1100

    add uniqueContainerN

--- src/main/scala/net/ssanj/scalacheck/ease/internal/UniqueContainerN.scala
+++ src/main/scala/net/ssanj/scalacheck/ease/internal/UniqueContainerN.scala
@@ -5,26 +5,27 @@ import org.scalacheck._
 trait UniqueContainerN {
 
   private def found[A](l: List[A], value: A, isEqual: (A, A) => Boolean): Boolean = {
-    l.foldLeft(false)( (a, v) => a || isEqual(v, value))
+    l.exists(isEqual(_, value))
   }
 
-  def uniqueContainerN[A](n: Int, ga: Gen[A], r: Int)(isEqual: (A, A) => Boolean): Gen[List[A]] = {
+  def uniqueContainerN[A](n: Int, ga: Gen[A], r: Int)(isEqual: (A, A) => Boolean, seed: rng.Seed): Gen[List[A]] = {
 
-    def doUniqueContainerN(retries: Int, results: List[A]): Gen[List[A]] = {
+    @annotation.tailrec
+    def doUniqueContainerN(p: Gen.Parameters, s: rng.Seed, retries: Int, results: List[A]): Gen[List[A]] = {
       if (results.length >= n) Gen.const(results)
       else {
-        for {
-          value   <- ga
-          isFound = found(results, value, isEqual)
-          updated <-  if (isFound) {
-                        if (retries <= 0) Gen.fail[List[A]]
-                        else doUniqueContainerN(retries - 1, results)
-                      } else doUniqueContainerN(retries, value +: results)
-        } yield updated
+        ga.apply(p, s) match {
+          case None => Gen.fail[List[A]]
+          case Some(value) =>
+            if (found(results, value, isEqual)) {
+              if (retries <= 0) Gen.fail[List[A]]
+              else doUniqueContainerN(p, s.next, retries - 1, results)
+            } else doUniqueContainerN(p, s.next, retries, value +: results)
+        }
       }
     }
 
-    doUniqueContainerN(r, List.empty[A])
+    Gen.parameterized(doUniqueContainerN(_, seed, r, List.empty[A]))
   }
 
 }
--- src/test/scala/net/ssanj/scalacheck/ease/UniqueContainerNProps.scala
+++ src/test/scala/net/ssanj/scalacheck/ease/UniqueContainerNProps.scala
@@ -6,13 +6,37 @@ import syntax._
 
 object UniqueContainerProps extends Properties("UniqueContainerN") {
 
-  private def genUniqueContainer[A: Arb](isEqual: (A, A) => Boolean): Gen[List[A]] = for {
-    size   <- Gen.choose(0, 100)
-    values <- G.uniqueContainerN(size, arb[A], size * 10)(isEqual)
-  } yield values
+  private def genUniqueContainer[A: Arb](isEqual: (A, A) => Boolean): Gen[List[A]] = Gen.sized(size => for {
+    values <- G.uniqueContainerN(size, arb[A], size * 10)(isEqual, rng.Seed.random)
+  } yield values)
 
-  property("should always contain unique elements") =
-    Prop.forAll(genUniqueContainer[Int]((v1, v2) => v1 == v2)) { values: List[Int] =>
-      values.toSet.size == values.length
-    }
+  private def genUniqueContainerWithSize[A: Arb](isEqual: (A, A) => Boolean): Gen[(List[A], Int)] = Gen.sized(size => for {
+    values <- G.uniqueContainerN(size, arb[A], size * 10)(isEqual, rng.Seed.random)
+  } yield (values, size))
+
+  private def assertUnqiueValues[A](values: List[A]): Prop = values.toSet.size == values.length
+
+  private def assertLength[A](values: List[A], size: Int): Prop = values.length == size
+
+  private def containerWithUniqueValuesProp[A: Arb]= Prop.forAll(genUniqueContainer[A](_ == _)) { assertUnqiueValues }
+
+  private def containerWithSizeProp[A: Arb] = Prop.forAll(genUniqueContainerWithSize[A](_ == _)) { (assertLength _).tupled }
+
+  property("should always contain unique elements for Ints") =
+    containerWithUniqueValuesProp[Int]
+
+  property("should always contain unique elements for Chars") =
+    containerWithUniqueValuesProp[Char]
+
+  property("should always contain unique elements for AnyVal") =
+    containerWithUniqueValuesProp[AnyVal]
+
+  property("should always return Int containers of the required size") =
+    containerWithSizeProp[Int]
+
+  property("should always return Char containers of the required size") =
+    containerWithSizeProp[Char]
+
+  property("should always return AnyVal containers of the required size") =
+    containerWithSizeProp[AnyVal]
 }

793fc9cf2c0c7f7cc5efbf7aeb75cd16b6165bf2
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-10-28 00:19:14 +1100

    remove type unnecessary type ascriptions #2

--- src/test/scala/net/ssanj/scalacheck/ease/UniqueListNSeedProps.scala
+++ src/test/scala/net/ssanj/scalacheck/ease/UniqueListNSeedProps.scala
@@ -9,22 +9,22 @@ object UniqueListNSeedProps {
   def properties: Properties = {
     new Properties("uniqueListNSeed") {
       property("should always return Int containers of the required size with Arb") =
-          containerWithSizedSeedArbProp[Int](G.uniqueListNSeed[Int])
+          containerWithSizedSeedArbProp(G.uniqueListNSeed[Int])
 
       property("should always return Char containers of the required size with Arb") =
-        containerWithSizedSeedArbProp[Char](G.uniqueListNSeed[Char])
+        containerWithSizedSeedArbProp(G.uniqueListNSeed[Char])
 
       property("should always return AnyVal containers of the required size with Arb") =
-        containerWithSizedSeedArbProp[AnyVal](G.uniqueListNSeed[AnyVal])
+        containerWithSizedSeedArbProp(G.uniqueListNSeed[AnyVal])
 
       property("should always return Int containers of the required size with Gen") =
-        containerWithSizedSeedGenProp[Int](G.uniqueListNSeed[Int])
+        containerWithSizedSeedGenProp(G.uniqueListNSeed[Int])
 
       property("should always return Char containers of the required size with Gen") =
-        containerWithSizedSeedGenProp[Char](G.uniqueListNSeed[Char])
+        containerWithSizedSeedGenProp(G.uniqueListNSeed[Char])
 
       property("should always return AnyVal containers of the required size with Gen") =
-        containerWithSizedSeedGenProp[AnyVal](G.uniqueListNSeed[AnyVal])
+        containerWithSizedSeedGenProp(G.uniqueListNSeed[AnyVal])
     }
   }
 
